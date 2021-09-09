782ce958f0592911facad247bd615e715f7aadba
Luiz Felipe da Silva Morais <82244432+Felipemorai@users.noreply.github.com>
2021-09-08 21:55:50 -0300

    Upgrade

--- main.js
+++ main.js
@@ -1,16 +1,26 @@
 const wrapper = document.querySelector(".wrapper"),
 inputPart = wrapper.querySelector(".input-part"),
 infoTxt = inputPart.querySelector(".info-txt"),
-inputField = inputPart.querySelector("input");
+inputField = inputPart.querySelector("input"),
+locationBtn = inputPart.querySelector("button");
 
 inputField.addEventListener("keyup", e => {
-    /* If user pressed enter botton and input value isn't empty */
+    /* If user pressed enter button and input value isn't empty */
     if(e.key == "Enter" && inputField.value != "") {
         requestApi(inputField.value);
     }
 });
 
+locationBtn.addEventListener("click", () => {
+    if(navigator.geolocation) { /* If browser support geolocation api */
+        navigator.geolocation.getCurrentPosition(onSuccess, onError)
+    } else {
+        alert("Your browser not support  geolocation api");
+    }
+})
+
 function requestApi(city){
+    let apiKey = 'ca126af951544574737763ad3a9d5d16';
     let api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
     infoTxt.innerText = "Getting Weather details...";
     infoTxt.classList.add("pending");
